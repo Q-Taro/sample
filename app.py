c2dd1bcd066f695f7923c26685c7be1b783cab7e
Tsuyoshi Ishii <itron@mac.com>
2024-10-11 15:41:45 +0900

    new app

--- app.py
+++ app.py
@@ -1,13 +1,30 @@
 import streamlit as st
+import os
+from dotenv import load_dotenv
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import HumanMessage
 
+load_dotenv()
 
 st.title("langchain-streamlit-app")
 
-prompt = st.chat_input("What is up?")
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+for message in st.session_state.messages:
+    with st.chat_messag(message["role"]):
+        st.markdown(message["content"])
+
+
+
+
+prompt=st.chat_input("What is up?")
 
 if prompt:
-    with st.chat_message("user"):
-        st.markdown(prompt)
-    with st.chat_message("assistant"):
-        response = "Konnichiha"
-        st.markdown(response)
+
+
+#with st.chat_message("assistant")
+#    chat = ChatOpenAI(model_name=os.environ["OPENAI_API_MODEL"]),
+#    temperature=os.environ["OPENAI_API_TEMPERATURE"])
+#messages = [HumanMessage(content=prompt)]
+
+# end of script
