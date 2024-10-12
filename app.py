45927478038bb2d6bec0df7069b857f74cf50117
Tsuyoshi Ishii <itron@mac.com>
2024-10-12 17:51:51 +0900

    debug

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.title("langchain-streamlit-app")
 if "messages" not in st.session_state:
     st.session_state.messages = []
 for message in st.session_state.messages:
-    with st.chat_messag(message["role"]):
+    with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
 
